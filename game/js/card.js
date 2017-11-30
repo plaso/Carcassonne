2af24190930c48f3d3a69d1051787cac4b8d6d3d
Pablo Laso <plasocortabitarte@gmail.com>
2017-11-30 20:16:23 +0100

    feat:drag method created

--- game/js/card.js
+++ game/js/card.js
@@ -2,6 +2,10 @@ function Card(type) {
   this.type = type;
 }
 
+Card.prototype.drag = function() {
+  $(".card").draggable();
+};
+
 Card.prototype.rotate = function() {
 
 };
--- game/js/game.js
+++ game/js/game.js
@@ -1,1 +1,7 @@
+var card = new Card();
 var board = new Board();
+
+$( document ).ready(function() {
+  board.createCells();
+  card.drag();
+});
